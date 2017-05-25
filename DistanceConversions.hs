5c996492dcaf634b9053cc252a2fd3fc801f86c9
Estanislau Trepat <estanis@etrepat.com>
2017-05-25 11:39:17 +0200

    Implement Ch7 exercises.

--- DistanceConversions.hs
+++ DistanceConversions.hs
@@ -2,16 +2,21 @@ module DistanceConversions
 ( yardsToFeet
 , feetToInches
 , inchesToCentimetres
+, chainsToYards
 ) where
 
 -- Define yards to feet
 yardsToFeet ::  Float -> Float
-yardsToFeet y = undefined
+yardsToFeet y = 3 * y
 
 -- Define feet to inches
 feetToInches :: Float -> Float
-feetToInches f = undefined
+feetToInches f = 12 * f
 
 -- Define inches to centimetres
 inchesToCentimetres :: Float -> Float
-inchesToCentimetres i = undefined
+inchesToCentimetres i = 2.54 * i
+
+-- Define chains to yards
+chainsToYards :: Float -> Float
+chainsToYards ch = 22 * ch
