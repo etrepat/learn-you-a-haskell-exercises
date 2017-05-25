78ce3cb1514caf642fe59a653944737ee54c6be2
Estanislau Trepat <estanis@etrepat.com>
2017-05-25 11:37:35 +0200

    Add cabal/stack config.
    Add cabal and stack configs. Use a dummy main module for now.
    
    This allows emacs intero to work properly with this project's
    modules.

---
+++ Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+-- Dummy main module so that cabal/stack don't complain.
+-- This is so that emacs's intero knows how to load modules properly
+main :: IO ()
+main = do
+  putStrLn "Dummy Main module placeholder"
