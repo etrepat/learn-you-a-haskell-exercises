cf83c2dac37f6202f87eac0db9f9463bb5f6c74e
Estanislau Trepat <estanis@etrepat.com>
2017-05-26 16:16:24 +0200

    Implement Ch9 *first* exercise

--- 09-input-and-output.hs
+++ 09-input-and-output.hs
@@ -3,8 +3,14 @@
  - The program arguments are simply printed to the standard output.
  - If the first argument is -n, this argument is not printed, and no trailing newline is printed
  -}
-  
-main = undefined
+import System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ("-n":xs) -> putStr $ unwords args
+    otherwise -> putStrLn $ unwords args
 
 {- Write a lottery number picker
  - This function should take a StdGen instance, and produce a list of six unique numbers between 1 and 49, in numerical order
